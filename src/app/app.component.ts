2bc5188bc4e5afc1a6fe420dd6e1df879747bd30
Tayna <taynasoares203@gmail.com>
2025-08-19 15:23:50 -0300

    subindo imagens

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portfolio';
+}
