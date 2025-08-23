a162436140b8c5cf4410253779f8031e49e12c26
Tayna <taynasoares203@gmail.com>
2025-08-22 21:15:39 -0300

    add copy in the buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  @ViewChild('textoParaCopiar') area!: ElementRef;
 
+  copiarTexto( elemento: HTMLElement) {
+    const texto = elemento.innerText;
+    navigator.clipboard.writeText(texto)
+      .then(() => {
+        console.log('Texto copiado');
+      })
+      .catch(err => {
+        console.error('Erro ao copiar texto: ', err);
+      });
+  }
 }
